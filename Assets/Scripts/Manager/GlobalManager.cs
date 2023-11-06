f5cd7201a30da060400a52542dc5e00386319ea5
Luke Kim <tjclear214@gmail.com>
2023-11-06 17:36:23 +0900

    Default connection setup

---
+++ Assets/Scripts/Manager/GlobalManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GlobalManagers : MonoBehaviour
+{
+    // Singleton variable
+    public static GlobalManagers Instance { get; private set; } = null;
+
+    [field: SerializeField] public NetworkRunnerController NetworkRunnerController { get; private set; }
+
+
+    private void Awake()
+    {
+        // Singleton pattern, Why use awake method? Cuz this is the boot strap for all managers
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Destroy the duplicate
+            Destroy(gameObject);
+        }
+    }
+}
