982d9ad615336f631c72514e884a6106e65d316b
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2024-01-04 11:33:26 +0900

    [feat] Sound & Haptic 제거

--- Assets/14.Test/Jinwoo/AudioSystem/SoundData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SoundData", menuName = "Audio/SoundData")]
-public class SoundData : ScriptableObject
-{
-    public AudioClip audioClip;
-    public float volume = 1.0f;
-    public float pitch = 1.0f;
-}
