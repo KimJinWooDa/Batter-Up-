e4f002d17b89f7e5ae7948aa675bb6a64cd3378d
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2024-01-04 11:58:42 +0900

    [feat] 오류수정

--- Assets/14.Test/Jinwoo/Sound/AudioManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AudioManager
-{
-    public void PlaySound(SoundData soundData, Vector3 position)
-    {
-        AudioSource.PlayClipAtPoint(soundData.audioClip, position, soundData.volume);
-    }
-}
