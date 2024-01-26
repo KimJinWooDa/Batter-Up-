0ff323693a1dc6a9a8ec0de3736b8d6420c3dc80
LINEARJUN <tjclear214@gmail.com>
2024-01-26 19:57:58 +0900

    player rig

---
+++ Assets/02.Scripts/Photon/Player/PlayerRig.cs
@@ -0,0 +1,45 @@
+﻿// System
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+
+// Fusion
+using Fusion;
+
+
+public class PlayerRig : NetworkBehaviour
+{
+    [Header("VR Rig")]
+    [SerializeField] private Transform rigHead = null;
+    [SerializeField] private Transform rigLeftHand = null;
+    [SerializeField] private Transform rigRightHand = null;
+
+    [Header("Model")]
+    [SerializeField] private Transform modelHead = null;
+    [SerializeField] private Transform modelLeftHand = null;
+    [SerializeField] private Transform modelRightHand = null;
+
+
+    public override void FixedUpdateNetwork()
+    {
+        base.FixedUpdateNetwork();
+
+        // Update the VR rig
+        if (HasInputAuthority)
+        {
+            if(rigHead == null || rigLeftHand == null || rigRightHand == null) return;
+            if(modelHead == null || modelLeftHand == null || modelRightHand == null) return;
+
+            modelHead.position = rigHead.position;
+            modelHead.rotation = rigHead.rotation;
+
+            modelLeftHand.position = rigLeftHand.position;
+            modelLeftHand.rotation = rigLeftHand.rotation;
+
+            modelRightHand.position = rigRightHand.position;
+            modelRightHand.rotation = rigRightHand.rotation;
+        }
+    }
+}
